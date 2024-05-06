af06693d38279a55f5c57ae329b671720c0e84ad
Roslyn10 <roslynclewis1004@gmail.com>
2024-05-06 13:33:47 +0200

    Fixing puppet-lint

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,13 @@
 # Uses Puppet to install flask from pip3
 
 
-package { "python3-pip":
+package { 'python3-pip':
     ensure  => installed,
 }
 
 
 exec { 'install_flask':
-      command => "/usr/bin/pip3 install Flask==2.2.0",
-      creates => "/usr/local/lib/python3.*/dist-packages/Flask-2.1.0.dist-info",
-      require => Package["python3-pip"],
+      command => '/usr/bin/pip3 install Flask==2.2.0',
+      creates => '/usr/local/lib/python3.*/dist-packages/Flask-2.1.0.dist-info',
+      require => Package['python3-pip'],
 }
