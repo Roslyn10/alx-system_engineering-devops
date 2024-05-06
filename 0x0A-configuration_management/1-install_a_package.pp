29bec710cd8499d87e02bfc47398b4d532ecb912
Roslyn10 <roslynclewis1004@gmail.com>
2024-05-06 13:40:51 +0200

    Fixing code in 1, from install-flask to flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -6,7 +6,7 @@ package { 'python3-pip':
 }
 
 
-exec { 'install_flask':
+exec { 'flask':
       command => '/usr/bin/pip3 install Flask==2.2.0',
       creates => '/usr/local/lib/python3.*/dist-packages/Flask-2.1.0.dist-info',
       require => Package['python3-pip'],
