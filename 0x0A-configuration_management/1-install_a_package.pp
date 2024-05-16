f11f0d6ce1bfcb353453822c51ab0e8ddb5a623f
Roslyn10 <roslynclewis1004@gmail.com>
2024-05-16 13:18:15 +0200

    Fixing the code

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # Uses Puppet to install flask from pip3
 
-package { 'flask':
-    ensure   => '2.1.0',
-    provider => 'pip3'
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask_and_werkzeug':
+  command => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==1.0.1',
+  creates => '/usr/local/lib/python3.*/dist-packages/Flask-2.1.0.dist-info',
+  require => Package['python3-pip'],
 }
