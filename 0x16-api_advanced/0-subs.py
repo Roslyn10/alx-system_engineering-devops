b520053f9b4ead9d9cfc80aff767f001b4a58948
Roslyn10 <roslynclewis1004@gmail.com>
2024-06-18 17:17:25 +0200

    fixing documentation

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-
-import requests
-
-
 """
 A function that queries the Reddit API and returns...
 the number of subscribers for a given subreddit
 """
+
+import requests
 
 
 def number_of_subscribers(subreddit):
