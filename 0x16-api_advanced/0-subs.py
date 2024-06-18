c27e6c53b413e760e21af90c518609b98d9a9e99
Roslyn10 <roslynclewis1004@gmail.com>
2024-06-18 17:09:48 +0200

    Adding the code for 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import requests 
+
+"""
+A function that queries the Reddit API and returns the number of subscribers for a given subreddit
+"""
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers in the subreddit"""
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
