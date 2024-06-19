0b082e384852bb5de96f0fa2915388800da2b3cd
Roslyn10 <roslynclewis1004@gmail.com>
2024-06-19 22:45:34 +0200

    Adding the code for 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,6 @@
 # Fixes Apache 500 error
 
-
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
