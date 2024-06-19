0a2ab2bf01cbe0ade139a697ab9c3cb6d7cb60b8
Roslyn10 <roslynclewis1004@gmail.com>
2024-06-19 12:41:42 +0200

    Fixing the pycodestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,10 +13,10 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
+
     if response.status_code == 404:
         return 0
-    
+
     data = response.json()
     subscribers = data['data']['subscribers']
     return subscribers
