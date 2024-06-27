4ce12e2ee62df98f51c9b33c38d8549b316238f3
Roslyn10 <roslynclewis1004@gmail.com>
2024-06-27 22:30:53 +0200

    Adding the path

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,4 +3,5 @@ exec { 'set_ulimit_and_restart_nginx':
   command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' /etc/default/nginx; service nginx restart\"",
   onlyif  => "grep -q '^ULIMIT=' /etc/default/nginx",
   require => Service['nginx'],
+  path    => '/usr/bin:/usr/sbin:/bin'
 }
