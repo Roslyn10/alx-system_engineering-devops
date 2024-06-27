16bfe7c988432a6b6df5764968819faec6dd8934
Roslyn10 <roslynclewis1004@gmail.com>
2024-06-27 22:32:31 +0200

    Removing unnecessary items

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,5 @@
 #Setting up Nginx to handle multiple requests
 exec { 'set_ulimit_and_restart_nginx':
   command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' /etc/default/nginx; service nginx restart\"",
-  onlyif  => "grep -q '^ULIMIT=' /etc/default/nginx",
-  require => Service['nginx'],
   path    => '/usr/bin:/usr/sbin:/bin'
 }
